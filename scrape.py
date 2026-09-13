919709ffc346bb27b0141130c6b2dbd74f5da9ef
HARSHIT KOTNALA <harshitkotnala5@gmail.com>
2026-09-13 14:45:05 +0100

    Add Coca-Cola Workday jobs

--- scrape.py
+++ scrape.py
@@ -65,6 +65,7 @@ ASHBY_COMPANIES = ['notion', 'linear', 'ramp', 'elevenlabs', 'openai', 'vercel',
 # ---------------------------------------------------------------------------
 
 WORKDAY_COMPANIES = [
+    ('Coca-Cola', 'coke', 'wd1', 'coca-cola-careers'),
     ("Cohesity", "cohesity", "wd5", "Cohesity_Careers"),
     ('Deutsche Bank', 'db', 'wd3', 'DBWebsite'),
 
@@ -262,6 +263,7 @@ def build_company_registry(include_cache: bool = False):
         "Nucleo": "workable",
         "Keelvar": "personio",
         "Cohesity": "workday",
+        "Coca-Cola": "workday",
     }
     status_by_key.update({_company_key(k): v for k, v in explicit_status_aliases.items()})
     for mapping in connector_maps:
